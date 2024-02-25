6ab36baba961af4641fb9b397d70da4336415a45
pamonge <pablomonge55@gmail.com>
2024-02-25 20:45:31 -0300

    COMMIT ORIGINAL

---
+++ alimenta_forma/core/models.py
@@ -0,0 +1,91 @@
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.db import models
+
+class User(AbstractUser):
+    user = models.CharField(max_length= 50, blank=True, unique=True, verbose_name='Usuario')
+    first_name = models.CharField(max_length=50,  blank=True, verbose_name='Nombre')
+    last_name = models.CharField(max_length=50, blank=True, verbose_name='Apellido')
+    email = models.EmailField(max_length=50, blank=True, unique=True, verbose_name='Email' )
+
+    class Meta:
+    	verbose_name = 'usuario'
+    	verbose_name_plural = 'usuarios'
+
+class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Usuario')
+    image = models.ImageField(default='defaultuser.png', upload_to='users/', verbose_name='imagen de perfil')# OJO!!! redefinir default y upload_to  
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    location = models.CharField(max_length=150, null=True, blank=True, verbose_name='Localidad')
+    telephone = models.CharField(max_length=50, null=True, blank=True, verbose_name='Teléfono')
+    created_by_admin = models.BooleanField(default=True, blank=True, verbose_name='Creado por Admin')
+    age = models.IntegerField(blank=True, verbose_name='Edad')
+    city = models.CharField(maxlength=50, blank=True, verbose_name='Localidad')
+    preferred_language = models.CharField(max_length=50, blank=True) # Ver si puede elegir cualquier idioma o está limitado, es necesario?
+
+    class Meta:
+        verbose_name = 'perfil'
+        verbose_name_plural = 'perfiles'
+        ordering = ['-id']
+
+    def __str__ (self):
+        return self.user.username
+
+def create_user_profile (sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+def save_user_profile (sender, instance, **kwargs):
+    instance.profile.save()
+    
+post_save.connect (create_user_profile, sender=User)
+post_save.connect (save_user_profile, sender=User)
+
+class Course (models.Model):
+    STATUS_CHOICES = (
+        ('i', 'En inscripción'),
+        ('p', 'En progreso'),
+        ('f', 'Finalizado')
+    )
+    
+    name = models.CharField(max_length=90, verbose_name='Nombre')
+    description = models.CharField(max_length=500, blank=True, null=True, verbose_name='Descripción')
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'groups__name': 'profesores'},verbose_name='Profesor')
+    class_quantity = models.PositiveIntegerField(default=0, verbose_name='Cantidad de clases')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='i', verbose_name='Estado')
+        
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos' 
+     
+class Announcement (models.Model):
+    STATUS_CHOICES = (
+        ('v', 'vigente'),
+        ('f', 'Finalizado')
+    )
+    name = models.CharField(max_length=90, verbose_name='Nombre')
+    description = models.CharField(max_length=500, blank=True, null=True, verbose_name='Descripción')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='i', verbose_name='Estado')
+    
+    class Meta:
+    	verbose_name = 'anuncio'
+    	verbose_name_plural = 'anuncios'
+    
+    def __str__(self):
+    	return self.name
+    	
+        
+class Registration (models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Curso')
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='students_registration', limit_choices_to={'groups__name': 'estudiantes'},verbose_name='Estudiante')
+    enabled = models.BooleanField(default=True, verbose_name='Alumno Regular')
+    
+    def __str__(self):
+        return f'{self.student.username} - {self.course.name}'
+    
+    class Meta:
+        verbose_name = 'Inscripcion'
+        verbose_name_plural = 'Inscripciones'
